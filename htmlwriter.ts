bbb43dec2ef5a94391862bcde72a28cdda0d0a09
Nick Hodges <nickhodges@gmail.com>
2021-12-18 19:51:42 -0500

    fleshing out constructor logic

--- htmlwriter.ts
+++ htmlwriter.ts
@@ -1,15 +1,27 @@
+import { CloseTagType, CanHaveAttributes, TagStateType } from './htmlwriter.types';
 import IHtmlWriter from './htmlwriterintf';
+import { StringIsEmpty } from './htmlwriter.utils';
 
 export default class HtmlWriter implements IHtmlWriter {
-  private html: string;
+  private _html: string;
+  private _currentTagName: string;
+  private _tagState: TagStateType
 
-  constructor() {
-    this.html = '';
+  constructor(aTagName: string, aCloseTagType: CloseTagType = CloseTagType.Normal) {
+    this._html = '';
+    if (StringIsEmpty(aTagName)){
+      throw new Error('aTagname parameter cannot be empty');
+    }
+    this.initializeHtml(aCloseTagType, aTagName);
   }
 
-  
+  initializeHtml(aCloseTagType: CloseTagType, aTagName: string) {
+    this._currentTagName = aTagName;
+    this._html = `<${aTagName}`;
+    this._tagState = TagStateType.BracketOpen;
+  }
 
   public get HTML(): string {
-    return this.html;
+    return this._html;
   }
 }
