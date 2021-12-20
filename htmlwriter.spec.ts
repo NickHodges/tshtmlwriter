bb1135f2e69bf65cc198635e7a46c26da1e59332
Nick Hodges <nickhodges@gmail.com>
2021-12-19 23:28:24 -0500

    adding TagMaker tests

--- htmlwriter.spec.ts
+++ htmlwriter.spec.ts
@@ -1,53 +1,69 @@
-import IHtmlWriter from './htmlwriterintf';
+import IHtmlWriter from './htmlwriter.interfaces';
 import Htmlwriter from './htmlwriter';
 import * as chai from 'chai';
-import { StringIsEmpty, StringIsEmptyWithTrim, StringIsNotEmpty, StringIsNotEmptyWithTrim } from './htmlwriter.utils';
+import { StringHelper, TagMaker } from './htmlwriter.utils';
 
 const expect = chai.expect;
 
-
 // Htmlwriter tests
 describe('IHTMLWriter', () => {
-  it('should have correct open tag', () => {
+  it('should have correct open tag when created', () => {
     const htmlWriter: IHtmlWriter = new Htmlwriter('blah');
     expect(htmlWriter.HTML).to.equal('<blah');
   });
+
+
+
 });
 
-describe('HtmlWriter StringIsEmpty', () => {
+// StringHelper tests
+
+describe('StringHelper.StringIsEmpty', () => {
   it('should be true when string has no characters', () => {
-    expect(StringIsEmpty('')).to.be.true;
+    expect(StringHelper.StringIsEmpty('')).to.be.true;
   });
   it('should be false when string is all spaces', () => {
-    expect(StringIsEmpty(' ')).to.be.false;
+    expect(StringHelper.StringIsEmpty(' ')).to.be.false;
   });
   it('should be false when string has content', () => {
-    expect(StringIsEmpty('blah')).to.be.false;
+    expect(StringHelper.StringIsEmpty('blah')).to.be.false;
   });
 });
 
-// Utilities tests
-
-describe('HtmlWriter StringIsNotEmpty', () => {
+describe('StringHelper.StringIsNotEmpty', () => {
   it('should be true when string has no characters', () => {
-    expect(StringIsNotEmpty('')).to.be.false;
+    expect(StringHelper.StringIsNotEmpty('')).to.be.false;
   });
   it('should be false when string is all spaces', () => {
-    expect(StringIsNotEmpty(' ')).to.be.true;
+    expect(StringHelper.StringIsNotEmpty(' ')).to.be.true;
   });
   it('should be false when string has content', () => {
-    expect(StringIsNotEmpty('blah')).to.be.true;
+    expect(StringHelper.StringIsNotEmpty('blah')).to.be.true;
   });
 });
 
-describe('HtmlWriter StringIsEmptyWithTrim', () => {
+describe('StringHelper.StringIsEmptyWithTrim', () => {
   it('should be true when string has spaces', () => {
-    expect(StringIsEmptyWithTrim('     ')).to.be.true;
+    expect(StringHelper.StringIsEmptyWithTrim('     ')).to.be.true;
   });
 });
 
-describe('HtmlWriter StringIsNotEmptyWithTrim', () => {
+describe('StringHelper.StringIsNotEmptyWithTrim', () => {
   it('should be true when string has spaces', () => {
-    expect(StringIsNotEmptyWithTrim('  dd   ')).to.be.true;
+    expect(StringHelper.StringIsNotEmptyWithTrim('  dd   ')).to.be.true;
+  });
+});
+
+describe('TagMaker', () => {
+  it('should create proper opening tag', () => {
+    expect(TagMaker.MakeOpenTag('blah')).to.equal('<blah>');
+  });
+
+  it('should create proper closing tag', () => {
+    expect(TagMaker.MakeCloseTag('blah')).to.equal('</blah>');
+  });
+
+  it('should create proper slash close tag', () => {
+    expect(TagMaker.MakeSlashCloseTag()).to.equal(' />');
   });
 });
