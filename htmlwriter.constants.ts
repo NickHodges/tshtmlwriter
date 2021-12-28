4986d5df4fcf8ace66a8ed42d89f3e0ee39ba58a
Nick Hodges <nickhodges@gmail.com>
2021-12-27 20:01:37 -0500

    Added a bunch of constants

--- htmlwriter.constants.ts
+++ htmlwriter.constants.ts
@@ -1,6 +1,90 @@
 // HTML Characters and HTML Entities
-export const cCloseBracket: string = '>';
-export const cOpenBracket: string = '<';
+
+export const cObject: string = 'object';
+export const cHTML = 'html';
+export const cHead = 'head';
+export const cBody = 'body';
+export const cBlockQuote = 'blockquote';
+export const cClass = 'class';
+export const cStyle = 'style';
+export const cID = 'id';
+export const cMeta = 'meta';
+export const cName = 'name';
+export const cContent = 'content';
+export const cform = 'form';
+export const cInput = 'input';
+export const cButton = 'button';
+
+export const cFieldSet = 'fieldset';
+export const cLegend = 'legend';
+export const cFrameset = 'frameset';
+export const cFrame = 'frame';
+export const cNoFrames = 'noframes';
+export const cMap = 'map';
+export const cArea = 'area';
+export const cAlt = 'alt';
+export const cBaseFont = 'basefont';
+export const cLink = 'link';
+export const cParam = 'param';
+export const cAction = 'action';
+export const cMethod = 'method';
+export const cValue = 'value';
+
+export const cCRLF = '\r\n';
+
+export const cAnchor = 'a';
+export const cHREF = 'href';
+export const cImage = 'img';
+export const cURL = 'url';
+export const cSource = 'src';
+export const cHardRule = 'hr';
+export const cBreak = 'br';
+export const cUnorderedList = 'ul';
+export const cOrderedList = 'ol';
+export const cListItem = 'li';
+export const cClear = 'clear';
+export const cType = 'type';
+export const cTable = 'table';
+export const cBorder = 'border';
+export const cCellPadding = 'cellpadding';
+export const cCellSpacing = 'cellspacing';
+export const cWidth = 'width';
+export const cHeight = 'height';
+export const cTableRow = 'tr';
+export const cTableData = 'td';
+export const cTableHead = 'thead';
+export const cTableBody = 'tbody';
+export const cTableFoot = 'tfoot';
+export const cTableHeader = 'th';
+export const cTitle = 'title';
+export const cScript = 'script';
+export const cNoScript = 'noscript';
+export const cIFrame = 'iframe';
+export const cBase = 'base';
+export const cTarget = 'target';
+export const cOption = 'option';
+export const cOptGroup = 'optgroup';
+export const cCaption = 'caption';
+export const cLabel = 'label';
+export const cTextArea = 'textarea';
+export const cSelect = 'select';
+export const cFor = 'for';
+export const cCols = 'cols';
+export const cRows = 'rows';
+export const cColGroup = 'colgroup';
+export const cCol = 'col';
+
+export const cDD = 'dd';
+export const cDT = 'dt';
+export const cDL = 'dl';
+
+export const cOpenBracket = '<';
+export const cCloseBracket = '>';
+export const cCloseSlashBracket = '/>';
+export const cComment = '!--';
+export const cCloseComment = '-->';
+export const cSpace = ' ';
+export const cEmptyString = '';
 
 // Error strings
 export const strCaptionMustBeFirst = 'A <caption> tag must be the very first tag after a <table> tag.';
