a2606a012ea08286e51526dcb7a1ea17fa7858f7
Nick Hodges <nickhodges@gmail.com>
2022-01-17 22:36:35 -0500

    Adding NotInHeadTagError class

--- htmlwriter.exceptions.ts
+++ htmlwriter.exceptions.ts
@@ -1,5 +1,3 @@
-
-
 export class NoClosingTagHTMLWriterError extends Error {
   constructor(message: string) {
     super(message);
@@ -12,5 +10,13 @@ export class TryingToCloseClosedTagError extends Error {
     super(message);
     this.name = 'TryingToCloseClosedTagError';
     Object.setPrototypeOf(this, TryingToCloseClosedTagError.prototype);
+  }
+}
+
+export class NotInHeadTagError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'NotInHeadTagError';
+    Object.setPrototypeOf(this, NotInHeadTagError.prototype);
   }
 }
